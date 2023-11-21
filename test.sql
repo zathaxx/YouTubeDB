69f8061aa24479f54ada2a21589c7e5f198e2dba
Daniel Tsang <dtsang80@uw.edu>
2023-11-20 20:49:19 -0800

    Deleted hotel table & added CHANNEL table

--- test.sql
+++ test.sql
@@ -1,6 +1,9 @@
-CREATE TABLE HOTEL(
-	hotelNo int NOT NULL,
-	hotelName varchar(255) NOT NULL,
-	city varchar(255),
-	PRIMARY KEY (hotelNo)
+CREATE TABLE CHANNEL(
+    channelID int NOT NULL,
+    channelSubs int NOT NULL,
+    channelAge int NOT NULL,
+    videoCount int NOT NULL,
+    channelName varchar(255) NOT NULL,
+    channelDescription varchar(255),
+    PRIMARY KEY (channelID);
 );
