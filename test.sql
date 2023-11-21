5b01ba064c8bc0a592d41499136942aa6e2dac2c
Daniel Tsang <dtsang80@uw.edu>
2023-11-20 21:04:14 -0800

    Added VIDEO, CATEGORY, and PLAYLIST tables

--- test.sql
+++ test.sql
@@ -7,3 +7,29 @@ CREATE TABLE CHANNEL(
     channelDescription varchar(255),
     PRIMARY KEY (channelID);
 );
+
+CREATE TABLE VIDEO(
+    videoID INT NOT NULL,
+    videoName varchar(255) NOT NULL,
+    uploadDate DATE NOT NULL,
+    likes int NOT NULL,
+    dislikes int NOT NULL,
+    duration int NOT NULL,
+    views int NOT NULL,
+    description varchar(255),
+    PRIMARY KEY (videoID);
+);
+
+CREATE TABLE CATEGORY(
+    categoryID INT NOT NULL,
+    categoryName INT NOT NULL,
+    categorySubs INT NOT NULL,
+    PRIMARY KEY (categoryID);
+);
+
+CREATE TABLE PLAYLIST(
+    playlistID INT NOT NULL,
+    playlistName INT NOT NULL,
+    numOfVideos INT NOT NULL,
+    PRIMARY KEY (playlistID);
+); 
