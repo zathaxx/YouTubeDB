05c0cf563188564f0016189f13e7fa038fb31e7a
Xuan Nguyen <72367149+xuanmn@users.noreply.github.com>
2023-11-20 21:28:51 -0800

    Co-authored-by: Xuan Nguyen <xuanmn@users.noreply.github.com> Co-authored-by: zathaxx <zathaxx@users.noreply.github.com> Added tables

--- youtube.sql
+++ youtube.sql
@@ -6,31 +6,64 @@ CREATE TABLE CHANNEL(
     videoCount int,
     channelName varchar(255) NOT NULL,
     channelDescription varchar(255),
-    PRIMARY KEY (channelID);
+    PRIMARY KEY (channelID)
 );
 
 CREATE TABLE VIDEO(
     videoID INT NOT NULL,
     videoName varchar(255) NOT NULL,
-    uploadDate DATE,
-    likes INT,
-    dislikes INT,
-    duration,
-    views INT,
+    videoUploadDate DATE,
+    videoLikes INT,
+    videoDislikes INT,
+    videoDuration,
+    videoViews INT,
     description varchar(255),
-    PRIMARY KEY (videoID);
+    PRIMARY KEY (videoID)
 );
 
 CREATE TABLE CATEGORY(
     categoryID INT NOT NULL,
     categoryName INT NOT NULL,
     categorySubs INT,
-    PRIMARY KEY (categoryID);
+    PRIMARY KEY (categoryID)
 );
 
 CREATE TABLE PLAYLIST(
     playlistID INT NOT NULL,
     playlistName INT,
     numOfVideos INT,
-    PRIMARY KEY (playlistID);
+    PRIMARY KEY (playlistID)
 ); 
+
+CREATE TABLE COMMENT(
+    
+);
+
+CREATE TABLE SPONSOR(
+    sponsorName varchar(255) NOT NULL,
+    sponsorWebsite VARCHAR(255) NOT NULL,
+    sponsorID IN
+    PRIMARY KEY (spon)orID
+
+);
+
+CREATE TABLE POST(
+    postID INT NOT NULL,
+    postDate DATE,
+    postDescritption varchar(255),
+    postIikes INT,
+    postDislikes INT,
+    PRIMARY KEY (postID)
+);
+
+CREATE TABLE PROMOTES(
+
+);
+
+CREATE TABLE CONTAINS(
+
+);
+
+CREATE TABLE CATEGORIZED_UNDER(
+    
+);
