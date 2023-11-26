a64a983cb1baed02e6dc2b65cc3e7ce837945a6b
Daniel Tsang <dtsang80@uw.edu>
2023-11-25 19:26:38 -0800

    Co-authored-by: Xuan Nguyen <xuanmn@users.noreply.github.com> Co-authored-by: zathaxx <zathaxx@users.noreply.github.com> Added tables & checked primary/foreign keys, and domain constraints

--- youtube.sql
+++ youtube.sql
@@ -1,69 +1,108 @@
 -- SQLBook: Code
+-- SQLBook: Code
 CREATE TABLE CHANNEL(
     channelID int NOT NULL,
     channelSubs int,
-    channelAge int,
+    channelAge DATE,
+    CHECK(channnelAge >= '2005-02-13'),
     videoCount int,
-    channelName varchar(255) NOT NULL,
-    channelDescription varchar(255),
+    channelName varchar(51) NOT NULL,
+    channelDescription varchar(510),
     PRIMARY KEY (channelID)
 );
 
 CREATE TABLE VIDEO(
     videoID INT NOT NULL,
-    videoName varchar(255) NOT NULL,
-    videoUploadDate DATE,
+    channelID int NOT NULL,
+    videoName varchar(101) DEFAULT 'Untitled Video',
+    videoUploadDate DATE, -- video date must be a date that is either on the same day or after the channel age.
+    CHECK(videoUploadDate >= '2005-02-13'),
     videoLikes INT,
-    videoDislikes INT,
-    videoDuration,
+    CHECK(videoLikes >= 0),
+    
+    videoDislike
+    CHECK(videoDislikes >= 0),
+    
+    videoDuration TIME,
+    CHECK(videoDuration >= 0),
     videoViews INT,
-    description varchar(255),
-    PRIMARY KEY (videoID)
+    CHECK(videoViews >= 0),
+    description varchar(510),
+    PRIMARY KEY (videoID),
+    FOREIGN KEY (channelID) REFERENCES CHANNEL(channelID) ON DELETE CASCADE
 );
 
 CREATE TABLE CATEGORY(
     categoryID INT NOT NULL,
-    categoryName INT NOT NULL,
+    categoryvarchar(101),
     categorySubs INT,
+    CHECK(categorySubs >= 0),
     PRIMARY KEY (categoryID)
 );
 
 CREATE TABLE PLAYLIST(
     playlistID INT NOT NULL,
-    playlistName INT,
+    channelID INT NOT NULL,
+    playlistName varchar(101) DEFAULT 'Untitled Playlist',
     numOfVideos INT,
-    PRIMARY KEY (playlistID)
+    CHECK(numOfVideos >= 0),
+    PRIMARY KEY (playlistID),
+    FOREIGN KEY (channelID) REFERENCES CHANNEL(channelID) ON DELETE CASCADE
 ); 
 
 CREATE TABLE COMMENT(
-    
+    commentID INT NOT NULL,
+    videoID INT NOT NULL,
+    channelID int NOT NULL,
+    commentDescription varchar(5001),
+    commentLikes INT,
+    CHECK(commentLikes >= 0),
+    commentDate DATE,
+    CHECK(commentDate >= '2005-02-13'), -- comment date must be a date that is either on the same day or after the video upload date.
+    PRIMARY KEY(commentID, videoID, channelID),
+    FOREIGN KEY(videoID) REFERENCES VIDEO(videoID) ON DELETE CASCADE,
+    FOREIGN KEY(channelID) REFERENCES CHANNEL(channelID) ON DELETE CASCADE
 );
 
 CREATE TABLE SPONSOR(
-    sponsorName varchar(255) NOT NULL,
-    sponsorWebsite VARCHAR(255) NOT NULL,
-    sponsorID IN
-    PRIMARY KEY (spon)orID
-
+    sponsorID INT NOT NULL,
+    sponsorName varchar(101) NOT NULL,
+    sponsorWebsite VARCHAR(255),
+    --Check for https:// and domain name
+    PRIMARY KEY (sponsorID)
 );
 
 CREATE TABLE POST(
     postID INT NOT NULL,
+    channelID INT NOT NULL,
     postDate DATE,
-    postDescritption varchar(255),
-    postIikes INT,
+    CHECK(postDate >= '2005-02-13'), -- post date must be a date that is either on the same day or after the channel age.
+    postDescritption varchar(510),
+    postLikes INT,
+    CHECK(postLikes >= 0),
     postDislikes INT,
+    CHECK(postDislikes >= 0),
     PRIMARY KEY (postID)
+    FOREIGN KEY (channelID) REFERENCES CHANNEL(channelID) ON DELETE CASCADE
 );
 
 CREATE TABLE PROMOTES(
-
+    videoID INT NOT NULL,
+    sponsorID INT NOT NULL,
+    FOREIGN KEY(videoID) REFERENCES VIDEO (videoID) ON DELETE CASCADE,
+    FOREIGN KEY(sponsor) REFERENCES SPONSOR (sponsorID) ON DELETE CASCADE
 );
 
 CREATE TABLE CONTAINS(
-
+    categoryID INT NOT NULL,
+    channelID INT NOT NULL
+    FOREIGN KEY (category) REFERENCES CATEGORY (categoryID) ON DELETE CASCADE,
+    FOREIGN KEY (channel) REFERENCES CHANNEL (channelID) ON DELETE CASCADE
 );
 
 CREATE TABLE CATEGORIZED_UNDER(
-    
+    channelID INT NOT NULL,
+    categoryID INT NOT NULL,
+    FOREIGN KEY(channelID) REFERENCES CHANNEL(channelID) ON DELETE CASCADE,
+    FOREIGN KEY(categoryID) REFERENCES CATEGORY(categoryID) ON DELETE CASCADE
 );
