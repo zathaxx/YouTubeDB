bf355d4de5b5e8b6ec0b2cab48b63bf902a86ffc
Zath <zathaxx@gmail.com>
2023-11-28 20:36:12 -0800

    Clean up script

--- youtube.py
+++ youtube.py
@@ -1,9 +1,11 @@
 import requests
 import json
+import re
 
-API_KEY = ""
+API_KEY = "" # Generate YouTube API key here
 BASE_URL = "https://www.googleapis.com/youtube/v3"
-YT_CHANNEL_ID = "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+YT_CHANNEL_ID = "" # Insert desired YouTube channel here
+
 
 def get_channel(channel_id):
     params = {
@@ -58,10 +60,13 @@ def get_video(video_id):
     v_details = items[0]["contentDetails"]
 
     title = v_snippet['title'].replace("'", '')
-    description = v_snippet['description'][0:25].replace("'", '')
+    title = clean_text(title)
+    description = v_snippet.get('description', '')[:25].replace("'", '')
+    description = clean_text(description)
     likes = v_stats.get('likeCount', 0)
 
     return (f"INSERT into VIDEO values ('{items[0]['id']}', '{v_snippet['channelId']}', '{title}', '{v_snippet['publishedAt'][0:10]}', {likes}, '{duration_to_hhmmss(v_details['duration'])}', {v_stats['viewCount']}, '{description}');")
+
 
     
 def duration_to_hhmmss(duration):
@@ -107,7 +112,7 @@ def get_latest_playlist(channel_id, max_results=1):
         sql.append(get_playlist(playlist))
     return sql
 
-def get_playlist_videos(playlist_id, max_results=50):
+def get_playlist_videos(playlist_id, max_results=5):
     params = {
         'part': 'snippet',
         'playlistId': playlist_id,
@@ -122,8 +127,17 @@ def get_playlist_videos(playlist_id, max_results=50):
 
     for video in items:
         v_snippet = video['snippet']['resourceId']['videoId']
-        playlist_videos.append(get_video(v_snippet))
+        video_sql = get_video(v_snippet)
+
+        # Check if video_sql is not empty
+        if video_sql:
+            playlist_videos.append(video_sql)
+        else:
+            print(f"Error: Empty result for video_id {v_snippet}.")
+
     return playlist_videos
+
+
 
 def get_playlist(playlist_id):
     params = {
@@ -140,9 +154,34 @@ def get_playlist(playlist_id):
     p_snippet = items[0]["snippet"]
     p_details = items[0]["contentDetails"]
     
-    return (f"INSERT into PLAYLIST values ('{items[0]['id']}', '{p_snippet['channelId']}', '{p_snippet['title']}');")
+    title = p_snippet['title']
+    title = clean_text(title)
 
-def get_top_comments(video_id, max_results=5):
+    return (f"INSERT into PLAYLIST values ('{items[0]['id']}', '{p_snippet['channelId']}', '{title}');")
+
+def clean_text(comment_text):
+    # Remove quotes
+    comment_text = comment_text.replace("'", "").replace('"', '')
+
+    # Remove emojis
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F700-\U0001F77F"  # alchemical symbols
+                           u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+                           u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+                           u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+                           u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+                           u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+                           u"\U00002702-\U000027B0"  # Dingbats
+                           u"\U000024C2-\U0001F251" 
+                           "]+", flags=re.UNICODE)
+    comment_text = emoji_pattern.sub(r'', comment_text)
+
+    return comment_text
+
+def get_top_comments(video_id, max_results=10):
     params = {
         'part': 'snippet',
         'videoId': video_id,
@@ -161,48 +200,19 @@ def get_top_comments(video_id, max_results=5):
     for comment in items:
         snippet = comment['snippet']['topLevelComment']['snippet']
         comment_text = snippet.get('textDisplay', 'No text available')
+        
+        comment_text = clean_text(comment_text)
+
+        display_name = snippet['authorDisplayName']
+        display_name = display_name.replace("'", '')
 
         top_comments_sql.append(
             f"INSERT INTO COMMENT VALUES "
-            f"({comment['id']}, {video_id}, '{snippet['authorChannelId']['value']}', "
+            f"('{comment['id']}', '{video_id}', '{display_name}', "
             f"'{comment_text[:100]}', {snippet['likeCount']}, '{snippet['publishedAt'][0:10]}');"
         )
 
     return top_comments_sql
-
-
-input_file_path = 'channelID40.txt'
-output_file_path = 'video_output.txt'
-
-try:
-    with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
-        for channel_id in input_file:
-            channel_id = channel_id.strip()  # Remove leading/trailing whitespaces
-            results = get_latest_videos(channel_id)
-            for result in results:
-                print(result)
-                output_file.write(result + '\n')
-except FileNotFoundError:
-    print(f"Error: The file '{input_file_path}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
-
-
-
-
-# channel = get_channel(YT_CHANNEL_ID)
-
-# print(channel)
-
-# playlists = get_latest_playlist(YT_CHANNEL_ID)
-
-# for playlist in playlists:
-#     print(playlist)
-
-# videos = get_latest_videos(YT_CHANNEL_ID)
-
-# for video in videos:
-#     print(video)
 
 
 
