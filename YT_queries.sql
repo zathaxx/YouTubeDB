968b955c4af82b0b86c97ff8eb3304f76368c5a6
xuanmn <xuanmn@uw.edu>
2023-12-09 15:51:45 -0800

    2 questions added

--- YT_queries.sql
+++ YT_queries.sql
@@ -1,3 +1,8 @@
 -- Queries for YT project 
+-- complex queries  = "utilizing more than three tables"
 
 -- Question #1
+-- List all videos that was made 5 years ago today, with at least 5 thousand views and has over 100 comments.  
+
+-- Question #2
+-- List all channels that have over 20k subscribers, that were created last year, including their most popular video with the playlist if the video is in one. 
