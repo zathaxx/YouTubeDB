d5d0f8ac2d6e7418e084b34cca3e8e3f8e8e798b
xuanmn <xuanmn@uw.edu>
2023-12-09 16:00:55 -0800

    v2

--- YT_queries.sql
+++ YT_queries.sql
@@ -2,7 +2,11 @@
 -- complex queries  = "utilizing more than three tables"
 
 -- Question #1
--- List all videos that was made 5 years ago today, with at least 5 thousand views and has over 100 comments.  
+--(PostDate, VideoViews, Comments)
+-- List all videos that was made 5 years ago today, with at least 1 thousand views and has over 5 comments.  
+
+Select Post_Date AS 'POST_DATE:', Video_views As 'Video_Views:', Comment 
+
 
 -- Question #2
 -- List all channels that have over 20k subscribers, that were created last year, including their most popular video with the playlist if the video is in one. 
