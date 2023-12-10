577fc43f55aae3bb9461488d561c474d263d547f
xuanmn <xuanmn@uw.edu>
2023-12-09 16:26:28 -0800

    WIP

--- YT_queries.sql
+++ YT_queries.sql
@@ -3,9 +3,16 @@
 
 -- Question #1
 --(PostDate, VideoViews, Comments)
--- List all videos that was made 5 years ago today, with at least 1 thousand views and has over 5 comments.  
+-- List all videos that was made 5 years ago today, with at least 1 thousand views made by a channel that has over 10 thousand subscriber
 
-Select Post_Date AS 'POST_DATE:', Video_views As 'Video_Views:', Comment 
+SELECT postDate AS 'POST_DATE:', videoViews AS 'Video_Views:', channelName AS 
+'Channel Name: ' 
+FROM VIDEO, CHANNEL, POST 
+WHERE 
+SELECT Count (*) subs 
+FROM subs.channel = channelSubs < 10000 ON videoViews 
+SELECT videoViews
+WHERE videoViews > 10000 AND postDate = postDate.year == 2019 JOIN postDate = postDate.month == 12
 
 
 -- Question #2
